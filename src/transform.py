a6039d9d6c57ada9f809bd4ac7e05c3a0705e9c7
Ben Wells-Day <bwellsd@mathworks.com>
2020-05-25 16:18:27 +0100

    add some config to the transformer and a postprocessor. This is pretty limited for now - the postprocessor just stops at the last period if it can find one, and the config is only used to increase the max_length to 100, which is just an arbitrary choice.

--- src/transform.py
+++ src/transform.py
@@ -16,18 +16,26 @@ class Test_Transform(Transform):
 class GPT2_Transform(Transform):
     ''' Generates responses using the gpt2 language model.'''
 
-    def __init__(self):
+    def __init__(self,
+    max_length = 20,
+    temperature = 1.0,
+    top_k = 0,
+    top_p = 0.9):
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
         self.model = GPT2LMHeadModel.from_pretrained("gpt2")
+        self.max_length = max_length
+        self.temperature = temperature
+        self.top_k = top_k
+        self.top_p = top_p
 
     def transform(self,str):
         enc = self.tokenizer.encode(str,add_special_tokens=False,return_tensors="pt")
         seq = self.model.generate(
             input_ids = enc,
-            max_length = 20+len(enc[0]),
-            temperature = 1.0,
-            top_k = 0,
-            top_p = 0.9,
+            max_length = self.max_length+len(enc[0]),
+            temperature = self.temperature,
+            top_k = self.top_k,
+            top_p = self.top_p,
             repetition_penalty = 1.0,
             do_sample = True,
             num_return_sequences = 1)
@@ -44,3 +52,17 @@ class GPT2_Transform(Transform):
             )
             gen.append(tot_seq)
         return " ".join(gen)
+
+class Compose_Transform(Transform):
+    def __init__(self, T1, T2):
+        self.T1 = T1
+        self.T2 = T2
+
+    def transform(self,str):
+        return self.T2.transform(self.T1.transform(str))
+
+class Postprocessor(Transform):
+    def transform(self,str):
+        lastperiod = str.rfind(".")
+        end = len(str) if lastperiod==-1 else lastperiod+1
+        return str[:end]
